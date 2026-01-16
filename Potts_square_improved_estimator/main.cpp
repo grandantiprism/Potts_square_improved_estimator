7e6a141ed85fa6a9a71153ab20bb5c01fc22df38
ADE <ackielilac.g1@gmail.com>
2026-01-16 16:49:05 +0900

    Initial Commit

---
+++ Potts_square_improved_estimator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Potts_square_improved_estimator
+//
+//  Created by Akihisa Tateishi on 2026/01/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
